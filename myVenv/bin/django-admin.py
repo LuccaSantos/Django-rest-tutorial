e710c34e1f033e61bb033aa1edec1e5d49be6df0
Luca Santos <santoslucas8599@gmail.com>
2021-02-14 12:52:45 -0300

    first commit

---
+++ myVenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/santos/NomadProject/remote-job/django-rest-tutorial/myVenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
